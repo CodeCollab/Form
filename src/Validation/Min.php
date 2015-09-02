461ef349cef639f1c7a2496e0577a6175bb4372e
PeeHaa <info@pieterhordijk.com>
2015-09-02 23:44:21 +0200

    Added min validator

---
+++ src/Validation/Min.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+/**
+ * Min field validator
+ *
+ * PHP version 7.0
+ *
+ * @category   CodeCollab
+ * @package    Form
+ * @subpackage Validation
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    See the LICENSE file
+ * @version    1.0.0
+ */
+namespace CodeCollab\Form\Validation;
+
+/**
+ * Min field validator
+ *
+ * @category   CodeCollab
+ * @package    Form
+ * @subpackage Validation
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class Min extends Generic
+{
+    /**
+     * @var float The minimum allowed value
+     */
+    private $minimumValue;
+
+    /**
+     * Creates instance
+     *
+     * @param float $minimumValue The minimum allowed value
+     */
+    public function __construct(float $minimumValue)
+    {
+        $this->minimumValue = $minimumValue;
+    }
+
+    /**
+     * Validates a form field value
+     */
+    public function validate(string $value)
+    {
+        if (!$value || $value >= $this->minimumValue) {
+            return;
+        }
+
+        $this->error['min'] = [$this->minimumValue];
+    }
+}
---
+++ test/Unit/Validation/MinTest.php
@@ -0,0 +1,66 @@
+<?php declare(strict_types=1);
+
+namespace CodeCollabTest\Unit\Form\Validation;
+
+use CodeCollab\Form\Validation\Min;
+
+class MinTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers CodeCollab\Form\Validation\Min::__construct
+     */
+    public function testImplementsCorrectInterface()
+    {
+        $validator = new Min(1);
+
+        $this->assertInstanceOf('CodeCollab\Form\Validation\Validator', $validator);
+    }
+
+    /**
+     * @covers CodeCollab\Form\Validation\Min::__construct
+     * @covers CodeCollab\Form\Validation\Min::validate
+     */
+    public function testValidateIsValidWithoutValue()
+    {
+        $validator = new Min(1);
+
+        $this->assertNull($validator->validate(''));
+        $this->assertTrue($validator->isValid());
+    }
+
+    /**
+     * @covers CodeCollab\Form\Validation\Min::__construct
+     * @covers CodeCollab\Form\Validation\Min::validate
+     */
+    public function testValidateIsValidLargerThanMin()
+    {
+        $validator = new Min(2);
+
+        $this->assertNull($validator->validate('3'));
+        $this->assertTrue($validator->isValid());
+    }
+
+    /**
+     * @covers CodeCollab\Form\Validation\Min::__construct
+     * @covers CodeCollab\Form\Validation\Min::validate
+     */
+    public function testValidateIsValidEqualsMin()
+    {
+        $validator = new Min(2);
+
+        $this->assertNull($validator->validate('2'));
+        $this->assertTrue($validator->isValid());
+    }
+
+    /**
+     * @covers CodeCollab\Form\Validation\Min::__construct
+     * @covers CodeCollab\Form\Validation\Min::validate
+     */
+    public function testValidateIsInvalidSmallerThanMin()
+    {
+        $validator = new Min(2);
+
+        $this->assertNull($validator->validate('1'));
+        $this->assertFalse($validator->isValid());
+    }
+}
