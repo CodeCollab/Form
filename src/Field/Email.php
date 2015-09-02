b0360e862163ae89163e237418497166867853bd
PeeHaa <info@pieterhordijk.com>
2015-09-03 00:52:54 +0200

    Added missing fields

---
+++ src/Field/Email.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+/**
+ * Email field
+ *
+ * PHP version 7.0
+ *
+ * @category   CodeCollab
+ * @package    Form
+ * @subpackage Field
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    See the LICENSE file
+ * @version    1.0.0
+ */
+namespace CodeCollab\Form\Field;
+
+use CodeCollab\Form\Validation\Email as EmailValidator;
+
+/**
+ * Email field
+ *
+ * @category   CodeCollab
+ * @package    Form
+ * @subpackage Field
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class Email extends Generic
+{
+    /**
+     * Creates instance
+     *
+     * @param string                             $name            The name of the field
+     * @param \CodeCollab\Validation\Validator[] $validationRules The validation rules
+     */
+    public function __construct(string $name, array $validationRules = [])
+    {
+        $validationRules[] = new EmailValidator;
+
+        parent::__construct($name, 'email', $validationRules);
+    }
+}
