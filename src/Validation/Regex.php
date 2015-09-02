39cd0d1d6419bce949a4888afc00c02c7ead3aeb
PeeHaa <info@pieterhordijk.com>
2015-09-03 00:00:23 +0200

    Added regex validator

---
+++ src/Validation/Regex.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+/**
+ * Regex field validator
+ *
+ * PHP version 7.0
+ *
+ * @category   CodeCollab
+ * @package    Form
+ * @subpackage Validation
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    See the LICENSE file
+ * @version    1.0.0
+ */
+namespace CodeCollab\Form\Validation;
+
+/**
+ * Regex field validator
+ *
+ * @category   CodeCollab
+ * @package    Form
+ * @subpackage Validation
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class Regex extends Generic
+{
+    /**
+     * @var string The pattern to match against
+     */
+    private $pattern;
+
+    /**
+     * Creates instance
+     *
+     * @param string $pattern  The pattern to match against
+     */
+    public function __construct(string $pattern)
+    {
+        $this->pattern = $pattern;
+    }
+
+    /**
+     * Validates a form field value
+     */
+    public function validate(string $value)
+    {
+        if (!$value || preg_match($this->pattern, $value)) {
+            return;
+        }
+
+        $this->error['regex'] = [];
+    }
+}
---
+++ test/Unit/Validation/RegexTest.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+namespace CodeCollabTest\Unit\Form\Validation;
+
+use CodeCollab\Form\Validation\Regex;
+
+class RegexTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers CodeCollab\Form\Validation\Regex::__construct
+     */
+    public function testImplementsCorrectInterface()
+    {
+        $validator = new Regex('/foo/');
+
+        $this->assertInstanceOf('CodeCollab\Form\Validation\Validator', $validator);
+    }
+
+    /**
+     * @covers CodeCollab\Form\Validation\Regex::__construct
+     * @covers CodeCollab\Form\Validation\Regex::validate
+     */
+    public function testValidateIsValidWithoutValue()
+    {
+        $validator = new Regex('/foo/');
+
+        $this->assertNull($validator->validate(''));
+        $this->assertTrue($validator->isValid());
+    }
+
+    /**
+     * @covers CodeCollab\Form\Validation\Regex::__construct
+     * @covers CodeCollab\Form\Validation\Regex::validate
+     */
+    public function testValidateIsValidMatches()
+    {
+        $validator = new Regex('/foo/');
+
+        $this->assertNull($validator->validate('foo'));
+        $this->assertTrue($validator->isValid());
+    }
+
+    /**
+     * @covers CodeCollab\Form\Validation\Regex::__construct
+     * @covers CodeCollab\Form\Validation\Regex::validate
+     */
+    public function testValidateIsInvalidDoesntMatch()
+    {
+        $validator = new Regex('/foo/');
+
+        $this->assertNull($validator->validate('bar'));
+        $this->assertFalse($validator->isValid());
+    }
+}
