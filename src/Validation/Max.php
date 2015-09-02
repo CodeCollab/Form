db7aa0ac9620880e0a56399b540c8040616d35c8
PeeHaa <info@pieterhordijk.com>
2015-09-02 23:32:34 +0200

    Added max validator

---
+++ src/Validation/Max.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+/**
+ * Max field validator
+ *
+ * PHP version 7.0
+ *
+ * @category   CodeCollab
+ * @package    Form
+ * @subpackage Validation
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    See the LICENSE file
+ * @version    1.0.0
+ */
+namespace CodeCollab\Form\Validation;
+
+/**
+ * Max field validator
+ *
+ * @category   CodeCollab
+ * @package    Form
+ * @subpackage Validation
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class Max extends Generic
+{
+    /**
+     * @var float The maximum allowed value
+     */
+    private $maximumValue;
+
+    /**
+     * Creates instance
+     *
+     * @param float $maximumValue The maximum allowed value
+     */
+    public function __construct(float $maximumValue)
+    {
+        $this->maximumValue = $maximumValue;
+    }
+
+    /**
+     * Validates a form field value
+     */
+    public function validate(string $value)
+    {
+        if (!$value || $value <= $this->maximumValue) {
+            return;
+        }
+
+        $this->error['max'] = [$this->maximumValue];
+    }
+}
---
+++ test/Unit/Validation/MaxTest.php
@@ -0,0 +1,66 @@
+<?php declare(strict_types=1);
+
+namespace CodeCollabTest\Unit\Form\Validation;
+
+use CodeCollab\Form\Validation\Max;
+
+class MaxTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers CodeCollab\Form\Validation\Max::__construct
+     */
+    public function testImplementsCorrectInterface()
+    {
+        $validator = new Max(1);
+
+        $this->assertInstanceOf('CodeCollab\Form\Validation\Validator', $validator);
+    }
+
+    /**
+     * @covers CodeCollab\Form\Validation\Max::__construct
+     * @covers CodeCollab\Form\Validation\Max::validate
+     */
+    public function testValidateIsValidWithoutValue()
+    {
+        $validator = new Max(1);
+
+        $this->assertNull($validator->validate(''));
+        $this->assertTrue($validator->isValid());
+    }
+
+    /**
+     * @covers CodeCollab\Form\Validation\Max::__construct
+     * @covers CodeCollab\Form\Validation\Max::validate
+     */
+    public function testValidateIsValidSmallerThanMax()
+    {
+        $validator = new Max(2);
+
+        $this->assertNull($validator->validate('1'));
+        $this->assertTrue($validator->isValid());
+    }
+
+    /**
+     * @covers CodeCollab\Form\Validation\Max::__construct
+     * @covers CodeCollab\Form\Validation\Max::validate
+     */
+    public function testValidateIsValidEqualsMax()
+    {
+        $validator = new Max(2);
+
+        $this->assertNull($validator->validate('2'));
+        $this->assertTrue($validator->isValid());
+    }
+
+    /**
+     * @covers CodeCollab\Form\Validation\Max::__construct
+     * @covers CodeCollab\Form\Validation\Max::validate
+     */
+    public function testValidateIsInvalidLargerThanMax()
+    {
+        $validator = new Max(1);
+
+        $this->assertNull($validator->validate('2'));
+        $this->assertFalse($validator->isValid());
+    }
+}
