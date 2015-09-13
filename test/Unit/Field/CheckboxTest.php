285094e8600df60bb3b6e36fd8f7c0745716fc3c
PeeHaa <info@pieterhordijk.com>
2015-09-13 17:33:40 +0200

    Fixed default value

--- test/Unit/Field/CheckboxTest.php
+++ test/Unit/Field/CheckboxTest.php
@@ -29,17 +29,4 @@ class CheckboxTest extends \PHPUnit_Framework_TestCase
 
         $this->assertSame('', $field->getValue());
     }
-
-    /**
-     * @covers CodeCollab\Form\Field\Checkbox::__construct
-     * @covers CodeCollab\Form\Field\Checkbox::getValue
-     */
-    public function testGetValueReturnsDefaultValueEvenWhenValueIsManuallySet()
-    {
-        $field = new Checkbox('checkboxfield', [], 'defaultvalue');
-
-        $field->setValue('custom value');
-
-        $this->assertSame('defaultvalue', $field->getValue());
-    }
 }
